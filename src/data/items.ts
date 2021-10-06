33c106d608e68420a054d97cfbab529cfc323b5c
Jaedson Alves <jaedsom.alves@gmail.com>
2021-10-06 17:01:11 -0300

    adding files to repo

---
+++ src/data/items.ts
@@ -0,0 +1,10 @@
+import {Item} from '../types/Item';
+
+export const items: Item[] = [
+
+    {date: new Date(2021,9,15), category:'food', title: 'Mcdonalds', value: 32.12},
+    {date: new Date(2021,9,15), category:'food', title: 'Mcdonalds', value: 32.12},
+    {date: new Date(2021,9,15), category:'rent', title: 'Aluguel', value: 500},
+    {date: new Date(2021,9,15), category:'salary', title: 'Salario do mes', value: 4000}
+
+];
